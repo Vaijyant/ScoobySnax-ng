5b1e2d6b3030dbea2db339be539333de28190c89
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2020-10-16 04:06:50 -0400

    updated routes and components

--- src/app/modules/hero/hero.module.ts
+++ src/app/modules/hero/hero.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroContainerComponent } from './hero-container/hero-container.component';
 import { HeroRoutingModule } from './hero-routing.module';
+import { Hero2Component } from './hero2/hero2.component';
 
 @NgModule({
-  declarations: [HeroListComponent],
+  declarations: [HeroContainerComponent, HeroListComponent, Hero2Component],
   imports: [
     CommonModule,
     HeroRoutingModule
